a5ee7911bda28bae5193f8f58e7e97c8010665f6
Gouseppe <fernandeitorc@gmail.com>
2025-11-28 13:18:53 -0400

    feat: add AuthDrawer.tsx

---
+++ src/features/auth/ui/react/AuthDrawer.tsx
@@ -0,0 +1,42 @@
+import { role } from "@features/auth/nanostore";
+import { useStore } from "@nanostores/react";
+import Drawer from "@shared/ui/react/Drawer";
+
+const SUPER_ADMIN_LINKS = [
+  {
+    label: "Crear usuarios",
+    href: "/privado/crear-usuarios",
+  },
+  {
+    label: "Carreras",
+    href: "/carreras",
+  },
+];
+const ADMIN_LINKS = [
+  {
+    label: "Carreras",
+    href: "/carreras",
+  },
+];
+const USER_LINKS = [
+  {
+    label: "Carreras",
+    href: "/carreras",
+  },
+];
+export const AuthDrawer = () => {
+  const $role = useStore(role);
+  return (
+    <Drawer
+      position="left"
+      width="w-96"
+      links={
+        $role === "superAdmin"
+          ? SUPER_ADMIN_LINKS
+          : $role === "admin"
+          ? ADMIN_LINKS
+          : USER_LINKS
+      }
+    ></Drawer>
+  );
+};
