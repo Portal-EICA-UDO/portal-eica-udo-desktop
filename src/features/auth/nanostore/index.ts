2271e1b2c1f5cb4c672ba31bf9718c7b518efc10
Gouseppe <fernandeitorc@gmail.com>
2025-12-01 14:42:20 -0400

    feat: add fullName atom

--- src/features/auth/nanostore/index.ts
+++ src/features/auth/nanostore/index.ts
@@ -3,3 +3,4 @@ import { atom } from "nanostores";
 export const email = atom<string>("");
 export const role = atom<string>("");
 export const name = atom<string>("");
+export const fullName = atom<string>("");
