0382f7992d7da55c63478045fecc8f7a26d6ab11
Cesar Velasquez <cova01072@gmail.com>
2026-02-12 15:55:44 -0400

    fix

--- src/widgets/Carreras/type/type.tsx
+++ src/widgets/Carreras/type/type.tsx
@@ -3,7 +3,19 @@ export type Carrera = {
     nombre: string;
     imagen_url: string;
     descripcion: string;
+    horario_url: string;
+    dependencias: DependencyWithStaff[];
 };
+
+export type DependencyWithStaff = {
+    id: number;
+    nombre: string;
+    staff: {
+        id: number;
+        nombre: string;
+        apellido: string;
+    };
+}
 
 export type MateriasMap = {
     id: number;
