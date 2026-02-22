e8967bd4cca4de355dd5c6355fa82a4c2fb69e9d
Gouseppe <fernandeitorc@gmail.com>
2026-02-22 19:39:41 -0400

    feat: add consts to features/staff

---
+++ src/features/staff/const/consts.ts
@@ -0,0 +1,34 @@
+export const STAFF_TYPES = {
+  ["Profesor Instructor"]: {
+    value: "Profesor Instructor",
+    label: "Profesor Instructor",
+  },
+  ["Profesor Asistente"]: {
+    value: "Profesor Asistente",
+    label: "Profesor Asistente",
+  },
+  ["profesor Asociado"]: {
+    value: "profesor Asociado",
+    label: "profesor Asociado",
+  },
+  ["Profesor Titular"]: {
+    value: "Profesor Titular",
+    label: "Profesor Titular",
+  },
+  ["Profesor Agregado"]: {
+    value: "Profesor Agregado",
+    label: "Profesor Agregado",
+  },
+};
+
+export const CONDITION_TYPES = {
+  ["Tiempo Completo"]: { value: "Tiempo Completo", label: "Tiempo Completo" },
+  ["Tiempo Convencional"]: {
+    value: "Tiempo Convencional",
+    label: "Tiempo Convencional",
+  },
+  ["Dedicación Exclusiva"]: {
+    value: "Dedicación Exclusiva",
+    label: "Dedicación Exclusiva",
+  },
+};
